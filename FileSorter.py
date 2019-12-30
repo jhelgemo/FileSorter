a6ae69dece79c888d18f42be0db1b2b5dcb60090
jhelgemo <johakimh@gmail.com>
2019-12-30 23:01:45 +0100

    Created variable for current username, and file list

--- FileSorter.py
+++ FileSorter.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import os
+
+
+username = os.getlogin()
+path = "C:/Users/" + username + "/Downloads/"
+fileList = os.listdir(path)
+
+for files in fileList:
+    print(files)
+
+
+
+
+
